85e477bc52874c595c3280873afb0f1534370379
Diego <dgarci34@ucsc.edu>
2018-01-31 20:38:45 -0800

    node base files

---
+++ geomap_node.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
