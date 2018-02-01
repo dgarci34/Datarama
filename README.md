3f2a3d395698a1fd4c854501f273d900acc645bf
Diego <dgarci34@ucsc.edu>
2018-02-01 11:55:35 -0800

    last update for winter quarter

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ To initialize d3 run: 'npm install' in this directory
 
 Datarama has chnaged to InfoDump to avoid confusion with other
 entities with the Datarama name.
+
+Due to the provocative nature of 'InfoDump' we will return under
+a different name
--- geomap_node.js
+++ geomap_node.js
@@ -1,8 +1,31 @@
-console.log("hello world");
-var http = require('http');
+const jsdom = require("jsdom");             //make node work like your js browser compiler
+const { JSDOM } = jsdom;
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+console.log("hello world");
+var http = require('http');     
+var d3 = require('d3');                     //make d3 available
+
+var server = http.createServer(function(request, response) {  //send initial write request to server
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello!</h1><p>You asked for <code>" +
+                 request.url + "</code></p>");
+  response.end();
+});   //try http://localhost:8000/hello
+
+
+
+/*var map = d3.geomap.choropleth()
+    .geofile('/d3-geomap/topojson/countries/USA.json')
+    .projection(d3.geo.albersUsa)
+    .column('2012')
+    .unitId('fips')
+    .scale(1000)
+    .legend(true);
+
+d3.csv('/data/venture-capital.csv', function(error, data) {
+    d3.select('#map')
+        .datum(data)
+        .call(map.draw, map);
+      });*/
+
+server.listen(8000);
